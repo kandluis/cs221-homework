f789782420739474da854a844c2d655879956e73
Luis Perez <luis.perez.live@gmail.com>
2018-10-14 13:56:17 -0700

    Finished problem 2b

---
+++ reconstruct/submission.py
@@ -0,0 +1,111 @@
+import shell
+import util
+import wordsegUtil
+
+############################################################
+# Problem 1b: Solve the segmentation problem under a unigram model
+
+class SegmentationProblem(util.SearchProblem):
+    def __init__(self, query, unigramCost):
+        self.query = query
+        self.unigramCost = unigramCost
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
+        return self.query
+        # END_YOUR_CODE
+
+    def isEnd(self, state):
+        # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
+        return state == ""
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 12 lines of code, but don't worry if you deviate from this)
+        return [(state[:i], state[i:], self.unigramCost(state[:i])) for i in range(1, len(state) + 1)]
+        # END_YOUR_CODE
+
+def segmentWords(query, unigramCost):
+    if len(query) == 0:
+        return ''
+    ucs = util.UniformCostSearch(verbose=0)
+    ucs.solve(SegmentationProblem(query, unigramCost))
+
+    # BEGIN_YOUR_CODE (our solution is 10 lines of code, but don't worry if you deviate from this)
+    return " ".join(ucs.actions)
+    # END_YOUR_CODE
+
+############################################################
+# Problem 2b: Solve the vowel insertion problem under a bigram cost
+
+class VowelInsertionProblem(util.SearchProblem):
+    def __init__(self, queryWords, bigramCost, possibleFills):
+        self.queryWords = queryWords
+        self.bigramCost = bigramCost
+        self.possibleFills = possibleFills
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        # State is previous word (with vowels) plus index of current word to process.
+        return (wordsegUtil.SENTENCE_BEGIN, 0)
+        # END_YOUR_CODE
+
+    def isEnd(self, state):
+        # BEGIN_YOUR_CODE (our solution is 5 lines of code, but don't worry if you deviate from this)
+        _, wordIndex = state
+        return wordIndex == len(self.queryWords)
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 16 lines of code, but don't worry if you deviate from this)
+        prevVowelWord, currWordIndex = state
+        currWord = self.queryWords[currWordIndex]
+        return [(currVowelWord,
+                 (currVowelWord, currWordIndex + 1),
+                 self.bigramCost(prevVowelWord, currVowelWord))
+                for currVowelWord in self.possibleFills(currWord) or {currWord}]
+        # END_YOUR_CODE
+
+def insertVowels(queryWords, bigramCost, possibleFills):
+    # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
+    ucs = util.UniformCostSearch(verbose=0)
+    ucs.solve(VowelInsertionProblem(queryWords, bigramCost, possibleFills))
+    return " ".join(ucs.actions)
+    # END_YOUR_CODE
+
+############################################################
+# Problem 3b: Solve the joint segmentation-and-insertion problem
+
+class JointSegmentationInsertionProblem(util.SearchProblem):
+    def __init__(self, query, bigramCost, possibleFills):
+        self.query = query
+        self.bigramCost = bigramCost
+        self.possibleFills = possibleFills
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def isEnd(self, state):
+        # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 23 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+def segmentAndInsert(query, bigramCost, possibleFills):
+    if len(query) == 0:
+        return ''
+
+    # BEGIN_YOUR_CODE (our solution is 11 lines of code, but don't worry if you deviate from this)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+############################################################
+
+if __name__ == '__main__':
+    shell.main()
